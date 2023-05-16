1cb4484016d5e17bd875b1fed51f9208bdb9069b
Deby Ayu Syakhira.github.io <113694766+DebyAyuSyakhira@users.noreply.github.com>
2023-05-16 21:47:59 +0800

    Add files via upload

---
+++ WIP_PA_VB-main/WIP_Final_Project/WIP_Final_Project/Karyawan_Main.vb
@@ -0,0 +1,61 @@
+﻿Imports MySql.Data.MySqlClient
+
+Public Class Karyawan_Main
+
+    Public Property Id As Integer
+
+    Private Sub Karyawan_Main_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Call koneksi()
+        btnBarang.PerformClick()
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Dispose()
+        LogReg.Show()
+    End Sub
+
+    Private Sub btnBarang_Click(sender As Object, e As EventArgs) Handles btnBarang.Click
+        pnCanvas.Controls.Clear()
+        topLabel.Text = "Manajemen Data Barang__________________"
+        btnBarang.BackColor = Color.Gray
+        btnTransaksi.BackColor = Color.Transparent
+        btnProfil.BackColor = Color.Transparent
+    End Sub
+
+    Private Sub btnTransaksi_Click(sender As Object, e As EventArgs) Handles btnTransaksi.Click
+        pnCanvas.Controls.Clear()
+
+        topLabel.Text = "Manajemen Data Transaksi__________________"
+        btnBarang.BackColor = Color.Transparent
+        btnTransaksi.BackColor = Color.Gray
+        btnProfil.BackColor = Color.Transparent
+    End Sub
+
+    Private Sub btnProfil_Click(sender As Object, e As EventArgs) Handles btnProfil.Click
+        pnCanvas.Controls.Clear()
+        topLabel.Text = "Profil_________________"
+        btnBarang.BackColor = Color.Transparent
+        btnTransaksi.BackColor = Color.Transparent
+        btnProfil.BackColor = Color.Gray
+        With Karyawan_Profil
+            .TopLevel = False
+            pnCanvas.Controls.Add(Karyawan_Profil)
+            .BringToFront()
+            .Show()
+        End With
+        CMD = New MySqlCommand("select * from tbakun join tbgawai on tbakun.email=tbgawai.email where id='" & Id & "'", CONN)
+        RD = CMD.ExecuteReader
+        RD.Read()
+        If RD.HasRows Then
+            Karyawan_Profil.txtNama.Text = RD.GetString(1)
+            Karyawan_Profil.txtMail.Text = RD.GetString(2)
+            Karyawan_Profil.txtAlamat.Text = RD.GetString(3)
+            Karyawan_Profil.lbGaji.Text = RD.GetString(7)
+            Karyawan_Profil.lbShift.Text = RD.GetString(8)
+            Karyawan_Profil.pbProfilePic.ImageLocation = CurDir() + "\gambar\profil\" + RD.GetString(6)
+        End If
+        RD.Close()
+    End Sub
+
+
+End Class
